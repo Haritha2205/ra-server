db2e9e92ea41be215bffff4f0e769fcf2323ca79
Haritha <64891791+Haritha2205@users.noreply.github.com>
2021-04-24 13:05:20 +0530

    modified table.js

--- table.js
+++ table.js
@@ -3,34 +3,31 @@ let mysql = require('mysql');
 let connInfo = {
     host : "localhost",
     user : "root",
-    password : "pakkukrish@22",
+    password : "",
     database : "RA1"
 }
 
 let con = mysql.createConnection(connInfo);
-var sql="select * from Candidates;"
-con.query(sql,function(err,data,fields){
-    
+var sql = "select * from Candidates;"
+con.query(sql, function(err, data, fields) {    
     if (err) throw err;
     console.dir(JSON.stringify(fields));
     console.dir(JSON.stringify(data));
-    console.log("=====================================================")
+    console.log("=====================================================");
 });
 
-var sql="select * from Employees;"
-con.query(sql,function(err,data,fields){
-    
+var sql = "select * from Employees;"
+con.query(sql, function(err, data, fields) {    
     if (err) throw err;
     console.dir(JSON.stringify(fields));
     console.dir(JSON.stringify(data));
-    console.log("=====================================================")
+    console.log("=====================================================");
 });
 
-var sql="select * from Trainee;"
-con.query(sql,function(err,data,fields){
-    
+var sql = "select * from Trainee;"
+con.query(sql, function(err, data, fields) {    
     if (err) throw err;
     console.dir(JSON.stringify(fields));
     console.dir(JSON.stringify(data));
-    console.log("=====================================================")
+    console.log("=====================================================");
 });
